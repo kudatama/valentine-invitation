0405d22f164dfb3153265e6236675445c68fd9d7
kudatama <122752012+kudatama@users.noreply.github.com>
2025-01-22 21:49:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function handleYesClick() {
 function resizeYesButton() {
   const computedStyle = window.getComputedStyle(yesButton);
   const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.5;
+  const newFontSize = fontSize * 1.35;
 
   yesButton.style.fontSize = `${newFontSize}px`;
 }
